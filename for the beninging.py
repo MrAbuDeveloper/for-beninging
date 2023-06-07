3a646975d006da3eeba65916606a85e994e1fb68
Abdurahmon <MrAbuDevJohnson@gmail.com>
2023-06-07 11:45:13 +0300

    for the beninging

---
+++ for the beninging.py
@@ -0,0 +1,63 @@
+# print("Hello World")   #komentariya - izoh - bu izohlab
+# print('Hello World')   #str - string - bu matn turi
+# print(1) # integer - butun sonlar
+# print(11.4)    # float - qoldiqli
+# print(True)   # bool - boolean - Haqiqat
+# print(False)  # bool - boolean - Yolg'on
+
+# Ctrl + /  -- bu belgilangan qatorlarni izoh qilib qoyadi
+
+# print(10 + 3)
+# print(2 * 3)
+# print(12 / 6)   # / bu bizaga float qaytaradi
+# print(12 // 3)   # bu faqat butun sonini qaytaradi
+# print(10 // 3)
+# print(10 // -3)
+# print(10 % 2)   # % bu butun bo'linmagan sonni ko'rsatadi
+# print(11 % 2)
+
+# print(2 ** 3)
+# print(9 ** 0.5)
+# print(9 ** (1/2))
+
+# print('hello' + ' ' + 'world')
+# strlarni strga qo'shish bu konkatenatsiya
+# print('hello ' * 3)
+# print('hello ' + str(3))
+
+# text = "Hello world"
+# print(text)
+# print(text)
+# print(text)
+# print(text)
+
+
+# name = 'Abdurahmon'
+# lastname = 'Qo'chqorov'
+# age = 23
+# print('My name is ' + name + ' ' + lastname + '. My age is ' + str(age))
+
+# name = input("Ismingizni kiriting: ") # input har doim str qabul qiladi
+# print('Assalomu alaykum ' + name)   ## " "  ''
+
+# a = input('birinchi sonni kiriting: ')
+# b = input('ikkinchi sonni kiriting: ')
+#
+# print(int(b) - int(a))
+
+### user_name
+### UserName
+### userName
+### number1
+#### 1number mumkin emas
+
+user_name = input('Ismingizni kiriting: ')
+user_lastname = input('Familiyangizni kiriting: ')
+user_age = input("Yoshingizni kiriting: ")
+# print("Sizning ism familiyangiz: " + user_name + ' ' +
+#       user_lastname + '. Va yoshingiz ' + user_age)
+print(f"Sizning ism familiyangiz: {user_name} {user_lastname}. Va yoshingiz {user_age}")
+
+
+
+
